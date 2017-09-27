debf0168da1dc42a5309c3dadec8a2c5c3944748
Levy-G <levy@thelevelcap.com>
2017-09-28 00:41:08 +0400

    9/28/2017 Update
    Just better.

--- customJS.js
+++ customJS.js
@@ -1,4 +1,5 @@
 // Declaring general vars.
+/*
 var team = window["someVar"];
 var winTotal1 = window["win1"];
 var barcelona;
@@ -9,14 +10,25 @@ var milani;
 var manUtd;
 var nonWins = 0;
 var lastWinTotal = 0;
+*/
+
+var lastMatchDay = 0;
 
 //Vars for arrays. Includes team name and total wins.
-var teamArray0 = [window["someVar"], window["win1"]];
-var teamArray1 = [window["someVar2"], window["win2"]];
-var teamArray2 = [window["someVar3"], window["win3"]];
-var teamArray3 = [window["someVar4"], window["win4"]];
+var teamArray0 = [window["team1"], window["win1"]];
+var teamArray1 = [window["team2"], window["win2"]];
+var teamArray2 = [window["team3"], window["win3"]];
+var teamArray3 = [window["team4"], window["win4"]];
+var teamArray4 = [window["team5"], window["win5"]];
+var teamArray5 = [window["team6"], window["win6"]];
+var teamArray6 = [window["team7"], window["win7"]];
+var teamArray7 = [window["team8"], window["win8"]];
+var teamArray8 = [window["team9"], window["win9"]];
+var teamArray9 = [window["team10"], window["win10"]];
+
 
 //Logic for assigning team variables to arrays. Dynamically updated.
+/*
 if (teamArray0[0] == "ბარსელონა") {
   barcelona = teamArray0;
 }
@@ -32,19 +44,92 @@ else if (teamArray3[0] == "ბარსელონა") {
   barcelona = teamArray3;
 }
 
-console.log(barcelona);
+else if (teamArray4[0] == "ბარსელონა") {
+  barcelona = teamArray4;
+}
 
+else if (teamArray5[0] == "ბარსელონა") {
+  barcelona = teamArray5;
+}
+
+else if (teamArray6[0] == "ბარსელონა") {
+  barcelona = teamArray6;
+}
+
+else if (teamArray7[0] == "ბარსელონა") {
+  barcelona = teamArray7;
+}
+
+else if (teamArray8[0] == "ბარსელონა") {
+  barcelona = teamArray8;
+}
+
+else if (teamArray9[0] == "ბარსელონა") {
+  barcelona = teamArray9;
+}
+*/
+
+function realMadridChecker() {
+  if (teamArray0[0] == "რეალი მადრიდი") {
+    realMadrid = teamArray0;
+  }
+  else if (teamArray1[0] == "რეალი მადრიდი") {
+    realMadrid = teamArray1;
+  }
+
+  else if (teamArray2[0] == "რეალი მადრიდი") {
+    realMadrid = teamArray2;
+  }
+
+  else if (teamArray3[0] == "რეალი მადრიდი") {
+    realMadrid = teamArray3;
+  }
+
+  else if (teamArray4[0] == "რეალი მადრიდი") {
+    realMadrid = teamArray4;
+  }
+
+  else if (teamArray5[0] == "რეალი მადრიდი") {
+    realMadrid = teamArray5;
+  }
+
+  else if (teamArray6[0] == "რეალი მადრიდი") {
+    realMadrid = teamArray6;
+  }
+
+  else if (teamArray7[0] == "რეალი მადრიდი") {
+    realMadrid = teamArray7;
+  }
+
+  else if (teamArray8[0] == "რეალი მადრიდი") {
+    realMadrid = teamArray8;
+  }
+
+  else if (teamArray9[0] == "რეალი მადრიდი") {
+    realMadrid = teamArray9;
+  }
+}
+
+//setInterval (realMadridChecker, 4000);
+
+
+
+/*
+setTimeout(function(){
+  console.log(realMadrid); }, 4000);
+*/
 //Logic for checking number of nonWins in a row.
   //Check current winTotal
   //if current winTotal > last winTotal
   //then nonWins++
-
+/*
 if (barcelona[1] > lastWinTotal){
   nonWins++;
   lastWinTotal = barcelona[1];
 }
-
+*/
 //Logic for HTML5 Notification.
+/*
 if (nonWins > 1) {
   new Notification ("Header", {
       body: team
@@ -52,3 +137,52 @@ if (nonWins > 1) {
 }
 
 console.log(lastWinTotal);
+*/
+
+function checkBarcelona(teamName) {
+    if (teamArray0[0] == teamName) {
+    barcelona = teamArray0;
+    }
+    else if (teamArray1[0] == "ბარსელონა") {
+      barcelona = teamArray1;
+    }
+    else if (teamArray2[0] == "ბარსელონა") {
+      barcelona = teamArray2;
+    }
+    else if (teamArray3[0] == "ბარსელონა") {
+      barcelona = teamArray3;
+    }
+    else if (teamArray4[0] == "ბარსელონა") {
+      barcelona = teamArray4;
+    }
+    else if (teamArray5[0] == "ბარსელონა") {
+      barcelona = teamArray5;
+    }
+    else if (teamArray6[0] == "ბარსელონა") {
+      barcelona = teamArray6;
+    }
+    else if (teamArray7[0] == "ბარსელონა") {
+      barcelona = teamArray7;
+    }
+    else if (teamArray8[0] == "ბარსელონა") {
+      barcelona = teamArray8;
+    }
+    else if (teamArray9[0] == "ბარსელონა") {
+      barcelona = teamArray9;
+    }
+}
+
+
+setInterval (checkBarcelona("ბარსელონა"), 3000);
+
+
+
+
+
+function matchDayChecker() {
+  if (window["currentMatchDay"] > lastMatchDay ) {
+    console.log("New DAY");
+    lastMatchDay++;
+  }
+}
+setInterval (matchDayChecker, 3000);
--- onSiteJS.js
+++ onSiteJS.js
@@ -1,50 +1,82 @@
 //Get current seasonID.
 
+var team = window["someVar"];
+var winTotal1 = window["win1"];
+var barcelona;
+var realMadrid;
+var erovnuli;
+var dinamoTbilisi;
+var milani;
+var manUtd;
+var nonWins = 0;
+var lastWinTotal = 0;
+
+
+
 //Function to fetch live data from VFL.
-function teamChecker() {
-    fetch('https://vfl3.betradar.com/vfl/feeds/?/betssoneuropebetvfl/ka/Europe:Paris/gismo/vfl_tournament_livetablebyseasonandround/1215776/1', { method: 'get' })
+function tableBuilder() {
+    fetch('https://vfl3.betradar.com/vfl/feeds/?/betssoneuropebetvfl/ka/Europe:Paris/gismo/vfl_tournament_livetablebyseasonandround/1218394/1', { method: 'get' })
         .then((response) => {
             return response.json();
         }).then((data) => {
-            window["someVar"] = data.doc[0].data.tablerows[0].team.name;
+            window["currentMatchDay"] = data.doc[0].data.currentround;
+
+            window["team1"] = data.doc[0].data.tablerows[0].team.name;
             document.getElementById("team1").innerHTML = data.doc[0].data.tablerows[0].team.name;
             window["win1"] = data.doc[0].data.tablerows[0].winTotal;
-            document.getElementById("winTotal").innerHTML = data.doc[0].data.tablerows[0].winTotal;
+            document.getElementById("winTotal1").innerHTML = data.doc[0].data.tablerows[0].winTotal;
 
-            window["someVar2"] = data.doc[0].data.tablerows[1].team.name;
+            window["team2"] = data.doc[0].data.tablerows[1].team.name;
             document.getElementById("team2").innerHTML = data.doc[0].data.tablerows[1].team.name;
             window["win2"] = data.doc[0].data.tablerows[1].winTotal;
             document.getElementById("winTotal2").innerHTML = data.doc[0].data.tablerows[1].winTotal;
 
-            window["someVar3"] = data.doc[0].data.tablerows[2].team.name;
+            window["team3"] = data.doc[0].data.tablerows[2].team.name;
+            document.getElementById("team3").innerHTML = data.doc[0].data.tablerows[2].team.name;
             window["win3"] = data.doc[0].data.tablerows[2].winTotal;
+            document.getElementById("winTotal3").innerHTML = data.doc[0].data.tablerows[2].winTotal;
 
-            window["someVar4"] = data.doc[0].data.tablerows[3].team.name;
+            window["team4"] = data.doc[0].data.tablerows[3].team.name;
+            document.getElementById("team4").innerHTML = data.doc[0].data.tablerows[3].team.name;
             window["win4"] = data.doc[0].data.tablerows[3].winTotal;
+            document.getElementById("winTotal4").innerHTML = data.doc[0].data.tablerows[3].winTotal;
 
-            window["someVar5"] = data.doc[0].data.tablerows[4].team.name;
+            window["team5"] = data.doc[0].data.tablerows[4].team.name;
+            document.getElementById("team5").innerHTML = data.doc[0].data.tablerows[4].team.name;
             window["win5"] = data.doc[0].data.tablerows[4].winTotal;
+            document.getElementById("winTotal5").innerHTML = data.doc[0].data.tablerows[4].winTotal;
 
-            window["someVar6"] = data.doc[0].data.tablerows[5].team.name;
+            window["team6"] = data.doc[0].data.tablerows[5].team.name;
+            document.getElementById("team6").innerHTML = data.doc[0].data.tablerows[5].team.name;
             window["win6"] = data.doc[0].data.tablerows[5].winTotal;
+            document.getElementById("winTotal6").innerHTML = data.doc[0].data.tablerows[5].winTotal;
 
-            window["someVar7"] = data.doc[0].data.tablerows[6].team.name;
+            window["team7"] = data.doc[0].data.tablerows[6].team.name;
+            document.getElementById("team7").innerHTML = data.doc[0].data.tablerows[6].team.name;
             window["win7"] = data.doc[0].data.tablerows[6].winTotal;
+            document.getElementById("winTotal7").innerHTML = data.doc[0].data.tablerows[6].winTotal;
 
-            window["someVar8"] = data.doc[0].data.tablerows[7].team.name;
+            window["team8"] = data.doc[0].data.tablerows[7].team.name;
+            document.getElementById("team8").innerHTML = data.doc[0].data.tablerows[7].team.name;
             window["win8"] = data.doc[0].data.tablerows[7].winTotal;
+            document.getElementById("winTotal8").innerHTML = data.doc[0].data.tablerows[7].winTotal;
 
-            window["someVar9"] = data.doc[0].data.tablerows[8].team.name;
+            window["team9"] = data.doc[0].data.tablerows[8].team.name;
+            document.getElementById("team9").innerHTML = data.doc[0].data.tablerows[8].team.name;
             window["win9"] = data.doc[0].data.tablerows[8].winTotal;
+            document.getElementById("winTotal9").innerHTML = data.doc[0].data.tablerows[8].winTotal;
 
-            window["someVar10"] = data.doc[0].data.tablerows[9].team.name;
+            window["team10"] = data.doc[0].data.tablerows[9].team.name;
+            document.getElementById("team10").innerHTML = data.doc[0].data.tablerows[9].team.name;
             window["win10"] = data.doc[0].data.tablerows[9].winTotal;
+            document.getElementById("winTotal10").innerHTML = data.doc[0].data.tablerows[9].winTotal;
         });
 }
 
-//document.getElementById("seasonid").innerHTML = g_srlive_selected_season;
 
 //Fetch live data from VFL every X second.
 //IMPORTANT. Need to write logic to fetch ONLY when there's a new matchday and ONLY between games. So far this is only a basic version.
-//setInterval(teamChecker, 3000);
-teamChecker();
+
+tableBuilder(); //Initial execution of the function.
+
+setInterval(tableBuilder, 3000);
